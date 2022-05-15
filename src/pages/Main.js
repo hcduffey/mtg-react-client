2b897e085b21ee52713b4ac133f44d43af56e4c5
hcduffey <cliff.duffey@pm.me>
2022-05-15 11:54:45 -0400

    defined functions to crud decks on database

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,11 +1,80 @@
 import { Routes, Route } from "react-router"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import DeckIndex from "./DeckIndex"
 import DeckDetail from "./DeckDetail"
 
 const Main = () => {
     //using local storage until backend is up
     const [decks, updateDecks] = useState([]);
+    const url = 'https://mtg-deck-backend.herokuapp.com/decks';
+
+    const syncDecks = () => {
+        fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            updateDecks(data);
+        })
+        .catch(err => console.log(err));
+    }
+
+    const showDeck = (id) => {
+        fetch(url + "/" + id)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+            return data;
+        })
+        .catch(err => console.log(err));
+    }
+
+    const createDeck = (deck) => {
+        fetch(url, {
+            method: "post",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(deck)
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+
+        syncDecks();
+    }
+
+    const updateDeck = (deck, id) => {
+        fetch(url + "/" + id, {
+            method: "put",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(deck)
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+
+        syncDecks();
+    }
+
+    const deleteDeck = (id) => {
+        fetch(url + "/" + id, {
+            method: "delete",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => console.log(data))
+        .catch((err) => console.log(err));
+
+        syncDecks();
+    }
+
+    useEffect(() => {
+        syncDecks();
+    }, []);
 
     return(
         <main>
